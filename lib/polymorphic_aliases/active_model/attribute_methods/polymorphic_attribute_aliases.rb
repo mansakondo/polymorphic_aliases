4cd5ae314cd3b41b550c375eaa802bb5972fa134
mansakondo <mansakondo22@gmail.com>
2021-07-27 13:12:53 +0200

    Update VALID_PREFIXES

--- lib/polymorphic_aliases/active_model/attribute_methods/polymorphic_attribute_aliases.rb
+++ lib/polymorphic_aliases/active_model/attribute_methods/polymorphic_attribute_aliases.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module PolymorphicAttributeAliases
-  VALID_PREFIXES  = %w( reload_ build_ create_ ).freeze
+  VALID_PREFIXES  = %w( reload_ ).freeze
   VALID_SUFFIXES  = %w( = ).freeze
 
   private_constant :VALID_PREFIXES, :VALID_SUFFIXES
