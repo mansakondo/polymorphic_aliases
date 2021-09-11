d06c6d24a423c240006ac8d84c1661bc24388653
mansakondo <mansakondo22@gmail.com>
2021-09-11 05:06:49 +0200

    Generate aliases when polymorphic associations are defined

--- lib/polymorphic_aliases.rb
+++ lib/polymorphic_aliases.rb
@@ -3,5 +3,4 @@ require "polymorphic_aliases/railtie"
 
 module PolymorphicAliases
   require "polymorphic_aliases/active_record/associations/builder/belongs_to_polymorphic_association.rb"
-  require "polymorphic_aliases/active_model/attribute_methods/polymorphic_attribute_aliases.rb"
 end
--- lib/polymorphic_aliases/active_record/associations/builder/belongs_to_polymorphic_association.rb
+++ lib/polymorphic_aliases/active_record/associations/builder/belongs_to_polymorphic_association.rb
@@ -9,7 +9,44 @@ module BelongsToPolymorphicAssociation
       valid += [:types] if options[:polymorphic]
       valid
     end
+
+    def define_accessors(model, reflection)
+      super
+
+      mixin = model.generated_association_methods
+
+      if reflection.polymorphic? && reflection.options[:types].present?
+        define_aliases(mixin, reflection)
+      end
+    end
+
+    def define_aliases(mixin, reflection)
+      types            = reflection.options[:types]
+      association_name = reflection.name
+
+      types.each do |type|
+        name = type.tableize.tr("/", "_").singularize
+        type = type.inspect
+
+        mixin.class_eval <<-CODE, __FILE__, __LINE__ + 1
+          def #{name}
+            if #{type} == self["#{association_name}_type"]
+              association(:#{association_name}).reader
+            end
+          end
+          def #{name}=(value)
+            if #{type} == self["#{association_name}_type"]
+              association(:#{association_name}).writer(value)
+            end
+          end
+          def reload_#{name}
+            if #{type} == self["#{association_name}_type"]
+              association(:#{association_name}).force_reload_reader
+            end
+          end
+        CODE
+      end
+    end
   end
 end
-
 ActiveRecord::Associations::Builder::BelongsTo.prepend(BelongsToPolymorphicAssociation)
